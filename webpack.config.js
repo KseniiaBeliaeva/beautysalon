c59d4892f55e111f6416ef2f8cbccf490cfe8c57
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2017-10-23 22:59:05 +1100

    Set up React

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: {
+        index: './src/index.jsx',
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [
+            // rules for modules (configure loaders, parser options, etc.)
+
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["react"] //"es2015"]
+                },
+            },
+        ],
+    },
+    plugins: [HtmlWebpackPluginConfig],
+};
+
